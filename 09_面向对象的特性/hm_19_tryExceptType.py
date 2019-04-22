25b318d68f4a52c12e74949e647cb0f2fd870aae
root <root@localhost.localdomain>
2019-04-23 07:28:36 +0800

    增加获取异常的代码示例

---
+++ 09_面向对象的特性/hm_19_tryExceptType.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: hm_19_tryExceptType.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-04-22 07:25:14
+# Last Modified: 2019-04-23 07:15:30
+################################################################# 
+
+try:
+	num = int(input("请输入一个整数："))
+	result = 8.0 / num
+	print result 
+except ZeroDivisionError:
+    print "0不能作为被除数，请更正！"
+except (NameError, SyntaxError):
+    print "请输入正确的整数！"
---
+++ 09_面向对象的特性/hm_20_obtainUnknownError.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: hm_20_obtainUnknownError.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-04-22 07:25:14
+# Last Modified: 2019-04-23 07:23:50
+################################################################# 
+
+try:
+	num = int(input("请输入一个整数："))
+	result = 8.0 / num
+	print result 
+except ZeroDivisionError:
+    print "0不能作为被除数，请更正！"
+except NameError:
+    print "请输入正确的整数！"
+# 输入 ！ 抛出的语法错误会走以下代码
+except Exception as result:
+    print "未知错误 %s" % result
