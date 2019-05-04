e4d38b41784ee4ef82091c8c7d258d9c74b00d39
root <root@localhost.localdomain>
2019-05-04 23:57:33 +0800

    增加模块发布相关的文件

---
+++ 11_模块/hm_11_releaseModule/build/lib/hm_message/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: __init__.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-04-25 06:45:01
+# Last Modified: 2019-04-25 07:03:06
+################################################################# 
+
+# 从当前目录导入模块列表
+
+from . import send_message
+from . import receive_message
---
+++ 11_模块/hm_11_releaseModule/hm_message/receive_message.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: receive_message.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-04-25 06:49:49
+# Last Modified: 2019-04-25 07:02:49
+################################################################# 
+
+def receive():
+    return "这是来自 100xx 的消息!" 
