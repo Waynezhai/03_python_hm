3453ff3771fd58bc6a1a8a35607bae49653d43e6
“Wayne_zhai” <zhyzhaihuiyan@163.com>
2019-05-19 20:45:47 +0800

    增加项目演练目录

---
+++ 13_项目演练/hm_01_pygameInstuction.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+################################################################# 
+# FileName: hm_01_pygameInstuction.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-05-19 20:16:08
+# Last Modified: 2019-05-19 20:43:57
+################################################################# 
+
+import pygame
+
+pygame.init()
+
+# 编写游戏的代码
+print "游戏代码..."
+
+pygame.quit()
+
