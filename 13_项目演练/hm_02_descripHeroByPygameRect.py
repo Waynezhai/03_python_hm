ed0f8904c7181d2daaa8ca3a98bdb0cbfa68944f
Wayne_zhy <zhyzhaihuiyan@163.com>
2019-05-22 00:02:52 +0800

    新增网络上搜索到的源码到代码库

--- 13_项目演练/hm_02_descripHeroByPygameRect.py
+++ 13_项目演练/hm_02_descripHeroByPygameRect.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-*- coding:utf8 -*-
-################################################################# 
+#################################################################
 # FileName: hm_02_descripHeroByPygameRect.py
 # Author: Wayne_zhy
 # Mail: zhyzhaihuiyan@163.com
 # Created Time: 2019-05-21 15:48:16
 # Last Modified: 2019-05-21 16:04:09
-################################################################# 
+#################################################################
 
 import pygame
 
@@ -15,4 +15,3 @@ hero_rect = pygame.Rect(100, 500, 120, 125)
 print "英雄的原点（%d, %d）" % (hero_rect.x, hero_rect.y)
 print "英雄的尺寸（%d, %d）" % (hero_rect.width, hero_rect.height)
 print "英雄的尺寸（%d, %d）" % hero_rect.size
-
