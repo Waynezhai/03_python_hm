526b0360af932d245715b94f9ef501fde6df2743
root <root@localhost.localdomain>
2019-05-23 20:36:56 +0800

    新增创建窗口和绘制图像文件---03/04

---
+++ 13_项目演练/hm_03_createWindow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+#################################################################
+# FileName: hm_03_createWindow.py
+# Author: Wayne_zhy
+# Mail: zhyzhaihuiyan@163.com
+# Created Time: 2019-05-23 18:59:56
+# Last Modified: 2019-05-23 19:01:29
+#################################################################
+
+import pygame
+
+pygame.init()
+
+# 创建游戏窗口
+screen = pygame.display.set_mode((480, 700))
+
+# 游戏循环---可以增加很多游戏控制
+while True:
+    pass
+
+pygame.quit()
